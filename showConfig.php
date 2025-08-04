9beee0f314df636e3c3ddcfb8e5196f5cc59d102
yousefhany697 <yousefhany697@gmail.com>
2025-08-04 03:00:03 +0300

    Create showConfig.php

---
+++ showConfig.php
@@ -0,0 +1,9 @@
+<?php
+// include 'config.php';
+// $getAllUsers = "SELECT * FROM users";
+// $result = $connect->query($getAllUsers);
+// while($row = $result->fetch_assoc()){
+//     echo 'the username is'  . $row['username'] . 'the email is' . $row['email'];
+//     echo '<br>';
+
+// }
